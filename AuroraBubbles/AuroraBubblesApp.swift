e2633c9e296699e9a61a5032a933ab6ddcb632b5
Aleksandar Milidrag <salkaorama@gmail.com>
2024-07-21 22:26:32 +0200

    Initial Commit

---
+++ AuroraBubbles/AuroraBubblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraBubblesApp.swift
+//  AuroraBubbles
+//
+//  Created by Aleksandar Milidrag on 21. 7. 2024..
+//
+
+import SwiftUI
+
+@main
+struct AuroraBubblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuroraBubbles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuroraBubbles
+//
+//  Created by Aleksandar Milidrag on 21. 7. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
